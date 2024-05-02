f3b98fc2003bc4900eca736f8ca80a4a91fee98d
“Aleksanteri” <aleksanteri.heliovaara@gmail.com>
2024-05-02 17:32:56 +0300

    datasetter fix

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, '/public/dist')));
 app.set('port', process.env.PORT || 8080);
+app.get("*", function (req, res) {
+	res.sendFile(path.join(__dirname, "/public/dist", "index.html"));
+});
 
 var server = app.listen(app.get('port'), function () {
 	console.log('listening on port ', server.address().port);
