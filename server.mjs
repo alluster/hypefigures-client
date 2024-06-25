3e9c6dadb3b34a70fbb081eb5a855ac9a7435d29
“Aleksanteri” <aleksanteri.heliovaara@gmail.com>
2024-06-25 15:27:13 +0300

    logos directory added to server

--- server.mjs
+++ server.mjs
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/public/dist')));
 app.use('/logos', express.static(path.join(__dirname, '/public/logos')));
+app.use('/integration-logos', express.static(path.join(__dirname, '/public/integration-logos')));
 
 app.set('port', process.env.PORT || 9500);
 
